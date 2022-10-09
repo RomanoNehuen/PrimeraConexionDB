ee83fe244c2fa5f6ab373fa5bc7f5fb678502272
RomanoNehuen <nehuenromano@gmail.com>
2022-10-09 11:49:51 -0300

    PrimeraConexionDB

---
+++ Primera conexionDB/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Primera_conexionDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemon = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemon
+            // 
+            this.dgvPokemon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemon.Location = new System.Drawing.Point(39, 26);
+            this.dgvPokemon.Name = "dgvPokemon";
+            this.dgvPokemon.Size = new System.Drawing.Size(333, 182);
+            this.dgvPokemon.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 295);
+            this.Controls.Add(this.dgvPokemon);
+            this.Name = "Form1";
+            this.Text = "Pokemons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemon;
+    }
+}
+
---
+++ Primera conexionDB/PokemonDatos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Primera_conexionDB
+{
+    internal class PokemonDatos
+    {
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+
+            {
+               
+
+                conexion.ConnectionString = "server =.\\SQLEXPRESS;database=Pokedex_DB;integrated security=true;";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Numero , Nombre , Descripcion from POKEMONS";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Numero = (int)lector["Numero"];
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    lista.Add(aux);
+
+                }
+                conexion.Close();
+
+                return lista;
+
+            }
+            
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }  
+}
